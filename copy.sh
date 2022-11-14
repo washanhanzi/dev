c8612db39b48c09f530b6c9ea3fc20ae75d8a470
frank <francismajere@gmail.com>
2022-11-14 11:40:12 +0800

    update kitty

--- copy.sh
+++ copy.sh
@@ -1,3 +1,4 @@
 cp -a $HOME/.config/terminal/alacritty/. $HOME/.config/alacritty/
 cp -a $HOME/.config/terminal/zellij/. $HOME/.config/zellij/
 cp -a $HOME/.config/terminal/zsh/.zshrc $HOME/.zshrc
+cp -a $HOME/.config/terminal/kitty/kitty.conf $HOME/.config/kitty/kitty.conf
