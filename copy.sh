ccfa805be37e848ab9193a278054a5e9a191c6bf
frank <francismajere@gmail.com>
2022-11-24 22:25:19 +0800

    update karabiner

--- copy.sh
+++ copy.sh
@@ -3,5 +3,4 @@
 cp -a $HOME/.config/terminal/zsh/.zshrc $HOME/.zshrc
 cp -a $HOME/.config/terminal/kitty/kitty.conf $HOME/.config/kitty/kitty.conf
 cp -a $HOME/.config/terminal/kitty/kitty.app.png $HOME/.config/kitty
-cp -a $HOME/.config/terminal/karabiner/assets/complex_modifications/. $HOME/.config/karabiner/assets/complex_modifications/
 cp -a $HOME/.config/terminal/sciprt/. $HOME/script/
---
+++ karabiner/copy.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cp -a $HOME/.config/terminal/karabiner/assets/complex_modifications/. $HOME/.config/karabiner/assets/complex_modifications/
