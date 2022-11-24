dc54c219a740a7a743b57067c469e107e8aca706
frank <francismajere@gmail.com>
2022-11-24 13:08:00 +0800

    update karabine

--- copy.sh
+++ copy.sh
@@ -1,4 +1,5 @@
-cp -a $HOME/.config/terminal/alacritty/. $HOME/.config/alacritty/
-cp -a $HOME/.config/terminal/zellij/. $HOME/.config/zellij/
+# cp -a $HOME/.config/terminal/alacritty/. $HOME/.config/alacritty/
+# cp -a $HOME/.config/terminal/zellij/. $HOME/.config/zellij/
 cp -a $HOME/.config/terminal/zsh/.zshrc $HOME/.zshrc
 cp -a $HOME/.config/terminal/kitty/kitty.conf $HOME/.config/kitty/kitty.conf
+cp -a $HOME/.config/terminal/karabiner/assets/complex_modifications/. $HOME/.config/karabiner/assets/complex_modifications/
